29e2d43d5e965e332dcb56c100878e055f5ebaee
Jacob Zimmerman <zimmerman.jake@gmail.com>
2017-02-27 15:15:27 -0500

    Don't set title

--- pure.zsh
+++ pure.zsh
@@ -75,7 +75,8 @@ prompt_pure_preexec() {
 
   # shows the current dir and executed command in the title when a process is active
   print -Pn "\e]0;"
-  echo -nE "$PWD:t: $2"
+  #echo -nE "$PWD:t: $2"
+  #echo -nE "$2"
   print -Pn "\a"
 }
 
@@ -86,7 +87,8 @@ prompt_pure_string_length() {
 
 prompt_pure_precmd() {
   # shows the full path in the title
-  print -Pn '\e]0;%~\a'
+  #print -Pn '\e]0;%~\a'
+  print -Pn '\e]0;\a'
 
   # default colors
   if [ -z "$PROMPT_PURE_DIR_COLOR" ]; then
